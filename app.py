49b15cfcf9b7368dc7dbe67d858b7fa3dfd9fe26
Will Smith <william.e.smithiv@gmail.com>
2022-03-13 18:53:39 -0400

    update with massey

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import pandas as pd
 import numpy as np
 
 from get_data import get_games
+from colley import colley
+from massey import massey
 
 games = get_games()
 teams = pd.read_csv("./2021_teams.csv")
@@ -13,11 +15,11 @@ num_teams = len(teams)
 method = st.selectbox("Choose a ranking method", [
                       "Colley", "Massey"], index=0, format_func=lambda x: x + " (accounts for point differential)" if x == "Massey" else x)
 home_win_weight = st.slider(
-    "Home win weight", value=1.0, min_value=0.0, max_value=10.0, step=0.1)
+    "Home win weight", value=1.0, min_value=0.0, max_value=2.0, step=0.01)
 away_win_weight = st.slider(
-    "Away win weight", value=1.0, min_value=0.0, max_value=10.0, step=0.1)
+    "Away win weight", value=1.0, min_value=0.0, max_value=2.0, step=0.01)
 neutral_win_weight = st.slider(
-    "Neutral win weight", value=1.0, min_value=0.0, max_value=10.0, step=0.1)
+    "Neutral win weight", value=1.0, min_value=0.0, max_value=2.0, step=0.01)
 
 use_time_weights = st.checkbox("Use time-based weights", value=True)
 
@@ -28,7 +30,27 @@ if use_time_weights:
         """)
         num_time_weights = st.number_input(
             "Number of time weights", value=2, min_value=2, max_value=10)
-        time_weights = np.array([1] * int(num_time_weights))
+        time_weights = [1] * int(num_time_weights)
         for i in range(int(num_time_weights)):
             time_weights[i] = st.slider(
-                f"Time weight {i+1}", value=1, min_value=0, max_value=10)
+                f"Time weight {i+1}", value=1.0, min_value=0.0, max_value=2.0, step=0.01)
+
+opts = {
+    "weight_home_win": home_win_weight,
+    "weight_away_win": away_win_weight,
+    "weight_neutral_win": neutral_win_weight,
+    "use_time_weights": use_time_weights,
+    "segment_weights": time_weights if use_time_weights else [1],
+}
+
+if method and method == "Colley":
+
+    colley_rankings = colley(games, teams, opts)
+
+    st.dataframe(colley_rankings, width=1500)
+    st.json(opts)
+
+if method and method == "Massey":
+    massey_rankings = massey(games, teams, opts)
+    st.dataframe(massey_rankings, width=1500)
+    st.json(opts)
---
+++ colley.py
@@ -0,0 +1,76 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+from math import ceil
+
+
+@st.cache
+def colley(games, teams, opts={
+    "weight_home_win": 1,
+    "weight_away_win": 1,
+    "weight_neutral_win": 1,
+    "use_time_weights": True,
+    "segment_weights": [1],
+}):
+
+    num_games = len(games)
+    num_teams = len(teams)
+
+    colley_matrix = 2 * np.diag(np.ones(num_teams))
+    b = np.ones(num_teams)
+    k = 10
+
+    day_before_season = games.loc[0, 'days_since_timestart'] - 1
+    last_day_of_season = games.loc[len(games) - 1, 'days_since_timestart']
+
+    for i in range(num_games):
+        [current_day, date, team_1_id, team_1_homefield, team_1_score,
+            team_2_id, team_2_homefield, team_2_score] = games.loc[i, :].values
+
+        team_1_index = team_1_id - 1
+        team_2_index = team_2_id - 1
+
+        num_segments = len(opts["segment_weights"])
+
+        weight_index = ceil(num_segments*((current_day - day_before_season) /
+                            (last_day_of_season - day_before_season))) - 1
+        time_weight = opts["segment_weights"][weight_index]
+
+        if team_1_score > team_2_score:
+            if team_1_homefield == 1:
+                game_weight = opts["weight_home_win"] * time_weight
+            elif team_1_homefield == -1:
+                game_weight = opts["weight_away_win"] * time_weight
+            else:
+                game_weight = opts["weight_neutral_win"] * time_weight
+        else:
+            if team_2_homefield == 1:
+                game_weight = opts["weight_home_win"] * time_weight
+            elif team_2_homefield == -1:
+                game_weight = opts["weight_away_win"] * time_weight
+            else:
+                game_weight = opts["weight_neutral_win"] * time_weight
+
+        # Update the colley matrix
+        colley_matrix[team_1_index, team_1_index] += game_weight
+        colley_matrix[team_2_index, team_2_index] += game_weight
+        colley_matrix[team_1_index, team_2_index] -= game_weight
+        colley_matrix[team_2_index, team_1_index] -= game_weight
+
+        # Update the b vector
+        if team_1_score > team_2_score:
+            b[team_1_index] += game_weight/2
+            b[team_2_index] -= game_weight/2
+        else:
+            b[team_1_index] -= game_weight/2
+            b[team_2_index] += game_weight/2
+
+    # Solve the system of equations
+    r = np.linalg.solve(colley_matrix, b)
+    iSort = np.argsort(-r)
+
+    colley_ratings = pd.DataFrame(columns=['rank', 'team', 'rating'])
+    for i in range(num_teams):
+        colley_ratings.loc[i, :] = [
+            i+1, teams.loc[iSort[i], 'team'], r[iSort[i]]]
+    return colley_ratings
